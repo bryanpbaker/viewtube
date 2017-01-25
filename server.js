eb5373232fc64764a919aa24769c191364dd6815
Bryan Baker <bryanpbaker@gmail.com>
2017-01-25 10:18:00 -0600

    updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req, res) => {
+app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
